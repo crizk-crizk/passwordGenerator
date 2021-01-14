710b1ae509b74e9357db135040be2741be99c680
Carolina R <carolinar@carolinas-mbp-2.myfiosgateway.com>
2021-01-14 15:41:13 -0500

    added variables in generatePassword

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-// Assignment Code. 
+// Assignment Code.
 //generate Button
 var generateBtn = document.querySelector("#generate");
 
 //this function returns the password
 function generatePassword() {
-  
+  var result = '';
+  var numerics = '0123456789';
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  var specialCharacters = '!@#$%';
+  let randomIndex = Math.floor(Math.random() * 26);
+  return upperCase[randomIndex];
 }
 
 // Write password to the #password input
@@ -13,7 +19,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
