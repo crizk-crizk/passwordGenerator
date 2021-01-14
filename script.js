a57f267ae540f516bc877e1c44be876ae9b62fe8
Carolina R <carolinar@carolinas-mbp-2.myfiosgateway.com>
2021-01-14 16:06:10 -0500

    added variables characters allowed

--- script.js
+++ script.js
@@ -4,16 +4,30 @@ var generateBtn = document.querySelector("#generate");
 
 //this function returns the password
 function generatePassword() {
-  var result = '';
-  var numerics = '0123456789';
-  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-  var specialCharacters = '!@#$%';
+  var result = "";
+  var numerics = "0123456789";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var specialCharacters = "!@#$%";
 
-  for (let i = 0; i < 8; i++) {
+  var numericsAllowed = false 
+  var upperCaseAllowed = false 
+  var lowerCaseAllowed = false 
+  var specialCharactersAllowed = false
+
+
+  var characterAmount = Number(prompt(
+    "How many charachters do you want in your password?"
+  ));
+
+  
+
+  console.log(typeof characterAmount);
+
+  for (let i = 0; i < characterAmount; i++) {
     let randomIndex = Math.floor(Math.random() * 26);
     result += upperCase[randomIndex];
-    console.log(result)
+    console.log(result);
   }
 
   return result;
