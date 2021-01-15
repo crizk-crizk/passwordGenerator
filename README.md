9e6af46b6b48ae674d20397ec5f4a88e5acd8291
Carolina R <carolinar@carolinas-mbp-2.myfiosgateway.com>
2021-01-14 21:32:03 -0500

    improved comments on .js code and cleaned up the layout

---
+++ README.md
@@ -0,0 +1,12 @@
+# JavaScript: Password Generator 
+
+## Description 
+* 
+*
+*
+
+## Links
+
+## Screenshots
+![](./screenshots/generatePASS-1.png)
+![](./screenshots/generatePASS-2.png)
--- script.js
+++ script.js
@@ -2,65 +2,71 @@
 //generate Button
 var generateBtn = document.querySelector("#generate");
 
-//this function returns the password
+//this function returns the password. 
 function generatePassword() {
+  // Variable declaration
   var result = "";
   var numerics = "0123456789";
   var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   var specialCharacters = "!@#$%";
 
+  //Find out how long the user wants the password to be
+  var characterAmount = Number(prompt(
+    "How many charachters do you want in your password?"
+  ));
+  //make sure that the number of characteres entered is within the valid range. Must have between 8 and 128 characters.
+  while(characterAmount < 8 || characterAmount > 128) {
+    characterAmount = Number(prompt(
+      "Please choose a number between 8 and 128."
+    ));
+  };
+  // Variables storing Booleans telling us which characters the user wants in their password
   var numericsAllowed = false 
   var upperCaseAllowed = false 
   var lowerCaseAllowed = false 
   var specialCharactersAllowed = false
 
-  var characterAmount = Number(prompt(
-    "How many charachters do you want in your password?"
-  ));
-  //make sure that the number of characteres entered is within the valid range
-  while(characterAmount < 8 || characterAmount > 128) {
-    characterAmount = Number(prompt(
-      "Please choose a number between 8 and 128."
-    ));
-  
-  }
-
   // validated at least one character type should be selected
   while (!numericsAllowed && !upperCaseAllowed && !lowerCaseAllowed && !specialCharactersAllowed) {
     //ask user what kind of characters they want in password
-    numericsAllowed = prompt('Do you want numbers in your password?') === 'yes' ? true : false;
-    upperCaseAllowed = prompt('Do you want upper case letters in your password?') === 'yes' ? true : false;
-    lowerCaseAllowed = prompt('Do you want lower case letters in your password?') === 'yes' ? true : false;
-    specialCharactersAllowed = prompt('Do you want special characters in your password?') === 'yes' ? true : false;
-    
+    numericsAllowed = prompt('Do you want numbers in your password? Answer yes or no.').toLowerCase() === 'yes' ? true : false;
+    upperCaseAllowed = prompt('Do you want upper case letters in your password? Answer yes or no.').toLowerCase() === 'yes' ? true : false;
+    lowerCaseAllowed = prompt('Do you want lower case letters in your password? Answer yes or no.').toLowerCase() === 'yes' ? true : false;
+    specialCharactersAllowed = prompt('Do you want special characters in your password? Answer yes or no.').toLowerCase() === 'yes' ? true : false;
+
+    //if user says 'no' to all the prompts, then alert to choose at least one type.
     if (!numericsAllowed && !upperCaseAllowed && !lowerCaseAllowed && !specialCharactersAllowed) {
     alert ('You must choose at least one of the character types.');
     }
-
   }
 
-
-  console.log(numericsAllowed, upperCaseAllowed, lowerCaseAllowed, specialCharactersAllowed);
-
+  //list of allowed characters to be included in the passwowrd. All allowed characters will be concatenated
   var allowedCharacters = '';
   if (numericsAllowed) {
-    allowedCharacters = numerics
+    allowedCharacters = numerics 
+    //console.log(allowedCharacters)
   }
   if (upperCaseAllowed) {
-    allowedCharacters = allowedCharacters + upperCase
+    allowedCharacters += upperCase
+    //console.log(allowedCharacters)
   }
   if (lowerCaseAllowed) {
-    allowedCharacters = allowedCharacters + lowerCase
-  }
+    allowedCharacters += lowerCase
+    //console.log(allowedCharacters)
+  } 
   if (specialCharactersAllowed) {
-    allowedCharacters = allowedCharacters + specialCharacters
+    allowedCharacters += specialCharacters
+    //console.log(allowedCharacters)
   }
 
-
-
+// Create length of password
   for (let i = 0; i < characterAmount; i++) {
+
+    // Computer comes up with a random number based on the length of the allowed characters by the user
     let randomIndex = Math.floor(Math.random() * allowedCharacters.length);
+    console.log(allowedCharacters[randomIndex])
+    //put together all the allowed characters as a long string to choose from
     result += allowedCharacters[randomIndex];
     console.log(result);
   }
